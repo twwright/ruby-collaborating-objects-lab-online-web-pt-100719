0f3a4e6d0a65fa82a60bfd2e738f2216d03893b1
Thomas Wright <twwright@gmail.com>
2019-11-11 20:23:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,21 @@ class Song
 	
 	def self.new_by_filename(filename)
 		files = filename.split(" - ")
-		song = Song.new(files[1])
+		song = self.new(files[1])
 		song.artist_name = files[0]
 		song
 	end
+	
+	def self.new_by_filename(filename)
+        artist_obj = Artist.all.select{|artist| artist.name == file_array[0]}
+        if artist_obj == [] 
+            artist_obj = Artist.new(file_array[0])
+        else
+            artist_obj = artist_obj.pop
+        end 
+        song.artist = artist_obj
+        song
+    end
 	
 	def artist_name=(artist)
 		artist.find_or_create_by_name(artist)
